0ec5c2c0f57653f248895d0772581d3c8838d028
mjshein <59715207+mjshein@users.noreply.github.com>
2020-10-21 09:24:40 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# node-govee-led
+ 
---
+++ index.js
@@ -0,0 +1,155 @@
+const axios = require('axios');
+
+class GoveeLED {
+	constructor(config) {
+		this.apiKey = config.apiKey
+		this.mac = config.mac
+		this.model = config.model
+		this.basePath = "https://developer-api.govee.com/v1/devices"
+	}
+
+	request(endpoint, reqData, method) {
+		if (this.mac === "") throw new Error("No MAC Address provided.");
+		if (this.model === "") throw new Error("No Model provided.");
+
+		let reqURL = this.basePath + endpoint;
+
+		var data = JSON.stringify(reqData);
+
+		var config = {
+			method: method,
+			url: reqURL,
+			headers: { 
+				'Govee-API-Key': this.apiKey, 
+				'Content-Type': 'application/json'
+			},
+			data: data
+		};
+
+		axios(config)
+		.then(function (response) {
+			return response.data;
+		})
+		.catch(function (error) {
+			throw new Error(error);
+		});
+	}
+
+	setColor(hexCode) {
+		var regex = /^#([0-9A-F]{3}){1,2}$/i;
+		if (!regex.test(hexCode)) throw new Error("Invalid Hex Color Code");
+		const hex2rgb = require('hex-rgb-conv');
+		var RGBconv = hex2rgb(hexCode, true);
+		var reqData = {
+		    "device": this.mac,
+		    "model": this.model,
+		    "cmd": {
+		        "name": "color",
+		        "value": {
+		            "r": RGBconv[0],
+		            "g": RGBconv[1],
+		            "b": RGBconv[2]
+		        }
+		    }
+		};
+		var endpoint = '/control';
+		return this.request(endpoint, reqData, "put");
+	}
+
+	setBrightness(brightnessLevel) {
+		if (!Number.isInteger(brightnessLevel)) throw new Error("Brightness Level Provided is Not A Number");
+		if (brightnessLevel > 100) throw new Error("Brightness Level Provided is Not From 1-100");
+		if (brightnessLevel < 1) throw new Error("Brightness Level Provided is Not From 1-100");
+		var reqData = {
+		    "device": this.mac,
+		    "model": this.model,
+		    "cmd": {
+		        "name": "brightness",
+		        "value": brightnessLevel
+		    }
+		};
+		var endpoint = '/control';
+		return this.request(endpoint, reqData, "put");
+	}
+
+	setColorTemperature(temperatureLevel) {
+		if (!Number.isInteger(temperatureLevel)) throw new Error("Temperature Level Provided is Not A Number");
+		if (temperatureLevel > 9000) throw new Error("Temperature Level Provided is Not From 2000-9000");
+		if (temperatureLevel < 2000) throw new Error("Temperature Level Provided is Not From 2000-9000");
+		var reqData = {
+		    "device": this.mac,
+		    "model": this.model,
+		    "cmd": {
+		        "name": "temperature",
+		        "value": temperatureLevel
+		    }
+		};
+		var endpoint = '/control';
+		return this.request(endpoint, reqData, "put");
+	}
+
+	turnOn() {
+		var reqData = {
+		    "device": this.mac,
+		    "model": this.model,
+		    "cmd": {
+		        "name": "turn",
+		        "value": "on"
+		    }
+		};
+		var endpoint = '/control';
+		return this.request(endpoint, reqData, "put");
+	}
+
+	turnOff() {
+		var reqData = {
+		    "device": this.mac,
+		    "model": this.model,
+		    "cmd": {
+		        "name": "turn",
+		        "value": "off"
+		    }
+		};
+		var endpoint = '/control';
+		return this.request(endpoint, reqData, "put");
+	}
+
+	getState() {
+		var reqData = {};
+		var endpoint = `/state?device=${this.mac}&model=${this.model}`;
+		return this.request(endpoint, reqData, "get");
+	}
+
+	async getDevices() {
+		var reqData = {};
+		var endpoint = ``;
+		let reqURL = this.basePath + endpoint;
+
+		var data = JSON.stringify(reqData);
+
+		var config = {
+			method: "get",
+			url: reqURL,
+			headers: { 
+				'Govee-API-Key': this.apiKey, 
+				'Content-Type': 'application/json'
+			},
+			data: data
+		};
+
+		let resData;
+
+		await axios(config)
+		.then(async function (response) {
+			resData = response.data.data;
+		})
+		.catch(function (error) {
+			throw new Error(error);
+		});
+
+		return resData;
+	}
+
+};
+
+module.exports = GoveeLED;
