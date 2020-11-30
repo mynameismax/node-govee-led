5cd11ca127311871a821919df5a74f3a01cf68bd
mjshein <59715207+mjshein@users.noreply.github.com>
2020-11-30 00:11:00 -0500

    built in function to convert hex to rgb instead of relying on package

--- index.js
+++ index.js
@@ -38,17 +38,31 @@ class GoveeLED {
 	setColor(hexCode) {
 		var regex = /^#([0-9A-F]{3}){1,2}$/i;
 		if (!regex.test(hexCode)) throw new Error("Invalid Hex Color Code");
-		const hex2rgb = require('hex-rgb-conv');
-		var RGBconv = hex2rgb(hexCode, true);
+		
+		function hexToRgb(hex) {
+			var shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
+			hex = hex.replace(shorthandRegex, function(m, r, g, b) {
+				return r + r + g + g + b + b;
+			});
+
+			var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+			return result ? {
+				r: parseInt(result[1], 16),
+				g: parseInt(result[2], 16),
+				b: parseInt(result[3], 16)
+			} : null;
+		}
+		
+		var RGBconv = hex2rgb(hexCode);
 		var reqData = {
 		    "device": this.mac,
 		    "model": this.model,
 		    "cmd": {
 		        "name": "color",
 		        "value": {
-		            "r": RGBconv[0],
-		            "g": RGBconv[1],
-		            "b": RGBconv[2]
+		            "r": RGBconv.r,
+		            "g": RGBconv.g,
+		            "b": RGBconv.b
 		        }
 		    }
 		};
